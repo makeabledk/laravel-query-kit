9144b4ced5b4fbb6769116ca33fc010ac019d79f
Rasmus Nielsen <rasmus@easify.dk>
2018-07-03 15:45:27 +0200

    Refactor stack

--- src/Builder/Stack.php
+++ src/Builder/Stack.php
@@ -3,7 +3,6 @@
 namespace Makeable\QueryKit\Builder;
 
 use Illuminate\Support\Collection;
-use Makeable\QueryKit\Contracts\OrConstraint;
 use Makeable\QueryKit\Contracts\QueryConstraint;
 
 class Stack
@@ -22,30 +21,6 @@ class Stack
     }
 
     /**
-     * @param Stack $stack
-     * @param $model
-     * @return bool
-     */
-    public static function check(self $stack, $model)
-    {
-        return $stack->tracks->first(function ($track) use ($model) {
-            return static::passesTrack($track, $model);
-        }) !== null;
-    }
-
-    /**
-     * @param Collection $track
-     * @param $model
-     * @return bool
-     */
-    protected static function passesTrack($track, $model)
-    {
-        return $track->first(function (QueryConstraint $constraint) use ($model) {
-            return ! $constraint->check($model);
-        }) === null;
-    }
-
-    /**
      * @param QueryConstraint $constraint
      * @param bool $or
      * @return Stack
@@ -57,16 +32,6 @@ class Stack
         }
 
         return $this->push($constraint);
-    }
-
-    /**
-     * @param $constraint
-     * @param $contract
-     * @return bool
-     */
-    protected function constraintImplements($constraint, $contract)
-    {
-        return array_key_exists($contract, class_implements($constraint));
     }
 
     /**
@@ -93,5 +58,29 @@ class Stack
         $current->push($constraint);
 
         return $this;
+    }
+
+    /**
+     * @param Stack $stack
+     * @param $model
+     * @return bool
+     */
+    public function check($model)
+    {
+        return $this->tracks->first(function ($track) use ($model) {
+            return static::passesTrack($track, $model);
+        }) !== null;
+    }
+
+    /**
+     * @param Collection $track
+     * @param $model
+     * @return bool
+     */
+    protected function passesTrack($track, $model)
+    {
+        return $track->first(function (QueryConstraint $constraint) use ($model) {
+            return ! $constraint->check($model);
+        }) === null;
     }
 }
